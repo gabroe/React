fb96cbde9c7f3b80d2151dc5919d8d65d88bf95e
Luis Dector <ldector@microstrategy.com>
2014-12-08 13:34:23 -0500

    expose test page as /api/test

---
+++ routes/api/test.js
@@ -0,0 +1,19 @@
+(function () {
+
+    var express = require('express'),
+        router = express.Router(),
+        path = require('path');
+
+    /* GET test page. */
+    router.get('/', function(req, res) {
+
+        //send the test html, nothing else to do
+        res.sendFile("test.html", {root: path.join(__dirname, '../../public')}) ;
+
+    });
+
+    module.exports = router;
+
+})();
+
+
