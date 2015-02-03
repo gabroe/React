4ed83d82fedf26f220ade46423098b8fb6c5e5be
Luis Dector <ldector@microstrategy.com>
2015-02-03 16:47:46 -0500

    Fencing Support

--- routes/api/dossiers.js
+++ routes/api/dossiers.js
@@ -9,6 +9,96 @@
         app = require('../../app'),
         debug = require('debug')('SiriusNode');
 
+    function validateDateFence(dateArray) {
+        var result = false,
+            currentDate = new Date();
+
+        if (dateArray && dateArray.length) {
+
+            dateArray.forEach(function (date) {
+                var dateMatchStart = true,
+                    dateMatchEnd = true,
+                    timeMatched = true;
+
+                if (date.start) {
+                    dateMatchStart = (currentDate >= (new Date(date.start)));
+                }
+
+                if (date.end) {
+                    dateMatchEnd = (currentDate <= (new Date(date.end)));
+                }
+
+                if (date.time) {
+                    timeMatched = validateTimeFence(date.time);
+                }
+
+                result |= dateMatchStart && dateMatchEnd && timeMatched;
+            });
+
+            return result;
+        }
+        return true;
+    }
+
+    function validateTimeFence(timeArray) {
+        var result = false,
+            currentTime = new Date();
+
+        if (timeArray && timeArray.length) {
+
+            timeArray.forEach(function (time) {
+                var timeRange = time.split("-");
+
+                if (timeRange.length === 2) {
+
+                    var startTimeRestriction = timeRange[0].split(":"),
+                        endTimeRestriction = timeRange[1].split(":"),
+                        startTime = new Date(),
+                        endTime = new Date(),
+                        setupTime = function (time, timeRestriction) {
+                            time.setHours(parseInt(timeRestriction[0], 10));
+                            time.setMinutes(parseInt(timeRestriction[1], 10));
+                        };
+
+
+                    if (startTimeRestriction.length >= 2 && endTimeRestriction.length >=2) {
+                        setupTime(startTime, startTimeRestriction);
+                        setupTime(endTime, endTimeRestriction);
+
+                        result |= (currentTime >= startTime && currentTime <= endTime);
+                    }
+                }
+            });
+            return result;
+        }
+        return true;
+    }
+
+    function validateIPFence(req, ipArray) {
+        var remoteIP = req.connection.remoteAddress,
+            regEx,
+            result = false;
+
+        if (remoteIP && ipArray && ipArray.length) {
+
+            ipArray.forEach(function (ip) {
+                regEx = new RegExp(ip.replace(".", "\\.").replace("*", "\\d*").replace("?", "\\d"));
+
+                result |= regEx.test(remoteIP);
+            });
+            return result;
+        }
+        return true;
+    }
+
+    function validateFencing(req, fence) {
+        if (!fence) {
+            return true;
+        }
+
+        return validateIPFence(req, fence.ip) && validateTimeFence(fence.time) && validateDateFence(fence.date);
+    }
+
     router.delete('/:name', function (req, res) {
 
         var mstrdb = app.get("mstrdb"),
@@ -55,6 +145,10 @@
 
             mstrdb.collection(MONGODB_COLLECTION_NAME).findOne({name: dossier.name}, function (err, result) {
                 if (!result) {
+
+                    dossier["time_created"] = (new Date()).toISOString();
+                    dossier["time_modified"] = dossier["time_created"];
+
                     mstrdb.collection(MONGODB_COLLECTION_NAME).insert(dossier, {w:1}, function (err, result) {
                         if (err) {
                             //something went wrong, return 400
@@ -80,9 +174,10 @@
     router.put('/:name', function (req, res) {
         var mstrdb = app.get("mstrdb"),
             dossierName = req.params.name,
-            dossier = req.body;
+            data = req.body,
+            timestampUpdate = {"time_modified": true};
 
-        if (dossierName === undefined) {
+            if (dossierName === undefined) {
             res.status(HTTP_STATUS.BAD_REQUEST).end();
             return;
         }
@@ -94,9 +189,16 @@
             return;
         }
 
-        if (dossier) {
+        if (data) {
 
-            mstrdb.collection(MONGODB_COLLECTION_NAME).update({name: dossierName}, dossier, {w:1}, function (err, result) {
+            if (data["service.shared"]) {
+                timestampUpdate["service.time_published"] = true;
+            }
+
+            mstrdb.collection(MONGODB_COLLECTION_NAME).update({name: dossierName}, {
+                $currentDate: timestampUpdate,
+                $set: data
+            }, {w:1}, function (err, result) {
                 if (err) {
                     //something went wrong, return 400
                     res.status(HTTP_STATUS.BAD_REQUEST).end();
@@ -148,10 +250,16 @@
                 //if the result is a collection of dossiers, convert it to array before sending the response back
                 if (result.toArray) {
                     result.toArray(function (err, dossiers) {
-                        res.json(dossiers);
+                        res.json(dossiers.filter(function (dossier) {
+                            return !dossier.service || !dossier.service.fence || validateFencing(req, dossier.service.fence)
+                        }));
                     });
                 } else {
-                    res.json(result);
+                    if (!result.service || !result.service.fence || validateFencing(req, result.service.fence)) {
+                        res.json(result);
+                    } else {
+                        res.json({"status":"error", "message":"outside fence"});
+                    }
                 }
 
             } else {
