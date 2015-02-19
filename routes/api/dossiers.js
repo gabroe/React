f277e62877bdaf5f518779c5e9d1a3aa74e8d092
Luis Dector <ldector@microstrategy.com>
2015-02-19 11:09:35 -0500

    Fence checking code cleanup

--- routes/api/dossiers.js
+++ routes/api/dossiers.js
@@ -9,7 +9,8 @@
         ObjectID = require('mongodb').ObjectID,
         router = express.Router(),
         app = require('../../app'),
-        debug = require('debug')('SiriusNode');
+        debug = require('debug')('SiriusNode'),
+        fenceChecker = require('../module/fenceChecker');
 
     function getUserAgent(req) {
         return req.headers['user-agent'];
@@ -27,103 +28,6 @@
                 }
         }
         return false;
-    }
-
-    function validateDateFence(dateArray) {
-        var result = false,
-            currentDate = new Date();
-
-        if (dateArray && dateArray.length) {
-
-            dateArray.forEach(function (date) {
-                var dateMatchStart = true,
-                    dateMatchEnd = true,
-                    timeMatched = true;
-
-                if (date.start) {
-                    dateMatchStart = (currentDate >= (new Date(date.start)));
-                }
-
-                if (date.end) {
-                    dateMatchEnd = (currentDate <= (new Date(date.end)));
-                }
-
-                if (date.time) {
-                    timeMatched = validateTimeFence(date.time);
-                }
-
-                result |= dateMatchStart && dateMatchEnd && timeMatched;
-            });
-
-            return result;
-        }
-        return true;
-    }
-
-    function validateTimeFence(timeArray) {
-        var result = false,
-            currentTime = new Date();
-
-        if (timeArray && timeArray.length) {
-
-            timeArray.forEach(function (time) {
-
-                if (time.split) {
-
-                    var timeRange = time.split("-");
-
-                    if (timeRange.length === 2) {
-
-                        var startTimeRestriction = timeRange[0].split(":"),
-                            endTimeRestriction = timeRange[1].split(":"),
-                            startTime = new Date(),
-                            endTime = new Date(),
-                            setupTime = function (time, timeRestriction) {
-                                time.setHours(parseInt(timeRestriction[0], 10));
-                                time.setMinutes(parseInt(timeRestriction[1], 10));
-                            };
-
-
-                        if (startTimeRestriction.length >= 2 && endTimeRestriction.length >=2) {
-                            setupTime(startTime, startTimeRestriction);
-                            setupTime(endTime, endTimeRestriction);
-
-                            result |= (currentTime >= startTime && currentTime <= endTime);
-                        }
-                    }
-                }
-            });
-            return result;
-        }
-        return true;
-    }
-
-    function validateIPFence(ipAddress, ipArray) {
-        var regEx,
-            result = false;
-
-        if (ipAddress && ipArray && ipArray.length) {
-
-            ipArray.forEach(function (ip) {
-                regEx = new RegExp(ip.replace(".", "\\.").replace("*", "\\d*").replace("?", "\\d"));
-
-                result |= regEx.test(ipAddress);
-            });
-            return result;
-        }
-        return true;
-    }
-
-    function validateFencing(ipAddress, dossier) {
-
-        var service = dossier && dossier.service,
-            fence = service && service.fence;
-
-        if (!fence) {
-            return true;
-        }
-
-        return validateIPFence(ipAddress, fence.ip) && validateTimeFence(fence.time) && validateDateFence(fence.date);
     }
 
     router.delete('/:name', function (req, res) {
@@ -322,12 +226,12 @@
             if (result.toArray) {
                 result.toArray(function (err, dossiers) {
                     //res.json(dossiers.filter(function (dossier) { //ignore fencing for now
-                    //    return validateFencing(req.connection.remoteAddress, dossier);
+                    //    return fenceChecker.validateFencing(req.connection.remoteAddress, dossier);
                     //}));
                     res.json(dossiers);
                 });
             } else {
-                if (isUserAgent(req, USER_AGENT_SIRIUS_WORKSHOP) || validateFencing(req.connection.remoteAddress, result)) {
+                if (isUserAgent(req, USER_AGENT_SIRIUS_WORKSHOP) || fenceChecker.validateFencing(req.connection.remoteAddress, result)) {
                     if (edge !== undefined && knownEdges.indexOf(edge) >= 0) {
 
                         var jsonResponse = {
